8ece0331756ed02edbc7bac71b42c0704de69426
Yogesh Kumar <kshanu385@gmail.com>
2025-09-16 12:33:07 +0530

    Repository Re-Structure

---
+++ MCA - 2C/Lecture-14/index.js
@@ -0,0 +1,42 @@
+// console.log(`Inside Index.js`);
+// var x;
+// console.log(x);
+// x = 23;
+// console.log(x);
+// x = `Hello World`;
+// console.log(x);
+
+// var z;
+// console.log(typeof z);
+
+// var y = null;
+// console.log(y);
+
+// var num = 12345678909876543211234567890n;
+// console.log(num);
+
+// var x = Symbol("name");
+// var y = Symbol("name");
+
+// console.log(x == y);
+// console.log(x === y);
+
+var str = 2 + "2";
+var str1 = 2 - "str";
+
+var a = 20 + "31" - 12 - true;
+
+var s = 2 + "2";
+let s1 = 2 - "2";
+
+// let s1 = "Hello World!!";
+
+console.log(s1);
+
+console.log(typeof s1.toString());
+
+console.log(typeof str1.toString());
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
